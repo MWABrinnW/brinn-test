0a1ea540f7620c0b246adad1ea9a7833bd081850
Grant Kilber <grant.kilber@marinerwealth.com>
2023-02-24 09:45:36 -0600

    adds record_date and record_datetime to fidelity models

--- macros/templates/fidelity/base/fidelity_nabase_103_business.sql
+++ macros/templates/fidelity/base/fidelity_nabase_103_business.sql
@@ -44,7 +44,7 @@ select
   , is_head
   , is_current
   , effective_date
-  , _source_loaded_at
+  , _source_loaded_at, _source_loaded_at::date as record_date, _source_loaded_at::timestamp as record_datetime
   , _source_file
 from {{ src }}
 {%- endmacro -%}
