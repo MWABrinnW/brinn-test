ff3bdb41c51181d82d7beda5442583f3c3c9b17f
Grant Kilber <grant.kilber@marinerwealth.com>
2023-02-24 10:19:44 -0600

    update fidelity raw views to restore legacy metadata fields

--- macros/templates/fidelity/raw/fidelity_raw_secmast_21.sql
+++ macros/templates/fidelity/raw/fidelity_raw_secmast_21.sql
@@ -7,7 +7,7 @@ select
   , nullif(trim(substring(content, 5, 1)), '')                                             as EXCHANGE_TRADED_FUND_INDICATOR
   , {{ col_is_head(reference=src) }}
   , {{ col_is_current(date_col='effective_date') }}
-  , effective_date
+  , effective_date, record_datetime::date as record_date, record_datetime::timestamp as record_datetime, source_file as source_file
   , record_datetime                                                                         as _source_loaded_at
   , source_file                                                                             as _source_file
 from {{ src }}
