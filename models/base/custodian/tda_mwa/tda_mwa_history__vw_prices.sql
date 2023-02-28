737fa8ee3a0b89c1ac8997d26a5af2e72d82a2b2
Grant Kilber <grant.kilber@marinerwealth.com>
2023-02-27 20:32:44 -0600

    updates legacy tda_mwa_history models with new sources

--- models/base/custodian/tda_mwa/tda_mwa_history__vw_prices.sql
+++ models/base/custodian/tda_mwa/tda_mwa_history__vw_prices.sql
@@ -1,11 +1,3 @@
-
-   select
-       value:c1::string         as symbol,
-       value:c2::string         as security_type,
-       value:c3::date           as date,
-       value:c4::decimal(19, 6) as price,
-       value:c5::string         as factor,
-       case when effective_date::date = (select max(effective_date::date) from {{ source('tda_mwa', 'prices') }}) then 1 else 0 end as is_current,
-       effective_date::date     as effective_date,
-       file_type::string        as file_type
-   from {{ source('tda_mwa', 'prices') }}
+select *
+from {{ ref('tda__base_pri') }}
+where rep_code_firm = 'mwa'
