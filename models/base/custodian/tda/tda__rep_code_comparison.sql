1c92ea71f60455c39a885095925fe0dca60ae95c
Grant Kilber <grant.kilber@marinerwealth.com>
2023-04-13 14:38:23 -0500

    updates tda__rep_code_comparison.sql

--- models/base/custodian/tda/tda__rep_code_comparison.sql
+++ models/base/custodian/tda/tda__rep_code_comparison.sql
@@ -26,20 +26,34 @@ with tmp_rep_codes as
         ,firm
     from {{ ref('tda_rep_codes') }}
 )
+,tmp_rep_codes_veo as
+(
+    select
+        rep_id as rep_code
+        ,sum(market_value) as market_value
+        ,count(distinct account_number) as accounts
+    from {{ ref('tda__base_accounts_balances') }}
+    where is_head = 1
+    group by 1
+)
 ,tmp_all_codes as
 (
     select distinct rep_code from tmp_rep_codes
     union
     select distinct rep_code from tmp_rep_codes_map
+    union
+    select distinct rep_code from tmp_rep_codes_veo
 )
 select
     a.rep_code
     ,case when b.rep_code is not null then 1 else 0 end as exists_in_datalake
     ,case when c.rep_code is not null then 1 else 0 end as exists_in_map
-    ,case when exists_in_datalake = 1 and exists_in_map = 1 then 1 else 0 end as exists_in_both
+    ,case when v.rep_code is not null then 1 else 0 end as exists_in_veo
     ,b.file_count
     ,b.min_effective_date as datalake_min_effective_date
     ,b.max_effective_date as datalake_max_effective_date
+    ,v.market_value         as veo_market_value
+    ,v.accounts             as accounts
     {# ,c.office_location      as map_office_location
     ,c.description          as map_description #}
 from tmp_all_codes a
@@ -47,5 +61,7 @@ left join tmp_rep_codes b
     on a.rep_code = b.rep_code
 left join tmp_rep_codes_map c
     on a.rep_code = c.rep_code
+left join tmp_rep_codes_veo v
+    on a.rep_code = v.rep_code
 where true
 order by a.rep_code
