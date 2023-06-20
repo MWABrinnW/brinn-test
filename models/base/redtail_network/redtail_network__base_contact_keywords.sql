89fe9c9a159c636fe16221a636272ba0a80b96b9
Allen Jones <122400605+mwaallen@users.noreply.github.com>
2023-06-20 16:08:14 -0500

    IT-11735 adds base_contact_keywords to redtail schema (#83)

---
+++ models/base/redtail_network/redtail_network__base_contact_keywords.sql
@@ -0,0 +1,13 @@
+select
+    json:id::int                              as udf_id
+  , json:keyword_id::int                      as keyword_id
+  , json:name::varchar(200)                   as name
+  , json:deleted::int                         as is_deleted
+  , json:created_at::timestamp                as created_at
+  , json:updated_at::timestamp                as updated_at
+  , {{ col_is_head(reference=source('redtail_network', 'contact_udfs'), reference_date_col='_effective_at::date', source_date_col='_effective_at::date') }}
+  , _effective_at::timestamp_ltz              as effective_at
+  , _effective_at::date                       as effective_date
+  , _created_at::timestamp_ltz                as _source_loaded_at
+  , _source_file::varchar(200)                as _source_file
+from {{ source('redtail_network', 'contact_keywords') }}
