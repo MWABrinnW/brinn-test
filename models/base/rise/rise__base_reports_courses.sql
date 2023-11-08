abdfe25064f632bcdd63c10b836c9a59787dde76
Allen Jones <122400605+allen-mwa@users.noreply.github.com>
2023-11-08 10:39:29 -0600

    adds course_id to base_reports_courses (#168)

--- models/base/rise/rise__base_reports_courses.sql
+++ models/base/rise/rise__base_reports_courses.sql
@@ -12,12 +12,13 @@ select a.content:completedAt:: timestampLTZ      as completedAt
      , a.content:userDeleted:: varchar(200)      as userDeleted
      , a.content:userId:: varchar(200)           as userId
      , a.content:userUrl:: varchar(200)          as userUrl
+     , split_part(_source_file, '/', 3)          as course_id
      , _source_file                              as _source_file
      , _created_at                               as _created_at
      , _extracted_at                             as _extracted_at
-     , case 
-          when _created_at = (select max(_created_at) from {{ source('rise', 'api_data') }} where _source_file like 'reports/courses/%') 
-          then 1 else 0 
+     , case
+          when _created_at = (select max(_created_at) from {{ source('rise', 'api_data') }} where _source_file like 'reports/courses/%')
+          then 1 else 0
           end                                    as is_head
 from {{ source('rise', 'api_data') }} a
 where _source_file like 'reports/courses/%'
