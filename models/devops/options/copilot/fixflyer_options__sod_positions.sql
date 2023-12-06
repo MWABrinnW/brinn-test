2e6ee5d88e9eb74c72144d7669ebad75b039e78e
Grant Kilber <116096178+grantith@users.noreply.github.com>
2023-12-06 08:00:21 -0600

    fix: [Flyer] adds missing lotnum field to sod_positions (#186)

--- models/devops/options/copilot/fixflyer_options__sod_positions.sql
+++ models/devops/options/copilot/fixflyer_options__sod_positions.sql
@@ -1,26 +1,27 @@
 select
-    effective_date    as effective_date
-    , custodian       as custodiancode
-    , account_number  as account
-    , product         as product
-    , ticker          as symbol
-    , quantity        as quantity
-    , price           as unitcost
-    , lot_cost        as totalcost
-    , current_price   as price
-    , lot_date        as lotdate
-    , null::text(200) as source_lot_id
-    , unsupervised    as unsupervised
-    , cusiplookup     as cusiplookup
-    , cusip           as cusip
-    , preferred       as preferred
-    , securityid      as securityid
-    , null::text(200) as security_type_description
-    , null::text(200) as fund_type
-    , null::text(200) as product_type
-    , null::text(200) as product_type_source_code
-    , null::text(200) as product_type_source_definition
-    , null::text(200) as legacy_product_type
-    , null::text(200) as legacy_product_type_source_code
-    , null::text(200) as legacy_product_type_source_definition
+    effective_date                          as effective_date
+    , custodian                             as custodiancode
+    , account_number                        as account
+    , product                               as product
+    , ticker                                as symbol
+    , quantity                              as quantity
+    , price                                 as unitcost
+    , lot_cost                              as totalcost
+    , current_price                         as price
+    , lot_date                              as lotdate
+    , lot_num                               as lotnum
+    , null::text(200)                       as source_lot_id
+    , unsupervised                          as unsupervised
+    , cusiplookup                           as cusiplookup
+    , cusip                                 as cusip
+    , preferred                             as preferred
+    , securityid                            as securityid
+    , security_type_description             as security_type_description
+    , fund_type                             as fund_type
+    , product_type                          as product_type
+    , product_type_source_code              as product_type_source_code
+    , product_type_source_definition        as product_type_source_definition
+    , legacy_product_type                   as legacy_product_type
+    , legacy_product_type_source_code       as legacy_product_type_source_code
+    , legacy_product_type_source_definition as legacy_product_type_source_definition
 from {{ ref('fixflyer_options__sod_positions_prep') }}
