bc59147d598f9e2c5c411d515c39b3c45ce0fc3b
Andrew Charlton <143548142+acharchar@users.noreply.github.com>
2023-12-14 16:47:54 -0600

    fix: [XCM] Update model typing (#192)

--- models/base/xcm/xcm__stg_client_detail.sql
+++ models/base/xcm/xcm__stg_client_detail.sql
@@ -1,21 +1,23 @@
 select
-    _pk
-    , json:"accountNumber"::string           as account_number
-    , json:"firstName"::string               as first_name
-    , json:"middleName"::string              as middle_name
-    , json:"last_Entity_Name"::string        as last_name
-    , json:"suffix"::string                  as suffix
-    , json:"getFullName"::string             as full_name
-    , json:"clientType"::string              as client_type
-    , json:"phoneNumber"::string             as phone_number
-    , json:"emailId"::string                 as email_id
-    , json:"accountNumber"::string           as account_number
-    , json:"originatingLocationName"::string as originating_location
-    , json:"active"::string                  as active
-    , json:"groupName"::string               as group_name
-    , json:"groupNumber"::string             as group_number
-    , json:"primaryTask"::string             as primary_task
-    , json:"periodEndDate"::string           as period_end_date
-    , _created_at
+    _pk::int                                                 as _pk
+    , json:"clientId"::int                                   as client_id
+    , NULLIF(json:"accountNumber"::text(200) , '')           as account_number
+    , NULLIF(json:"firstName"::text(200) , '')               as first_name
+    , NULLIF(json:"middleName"::text(200) , '')              as middle_name
+    , NULLIF(json:"last_Entity_Name"::text(200) , '')        as last_name
+    , NULLIF(json:"suffix"::text(200) , '')                  as suffix
+    , NULLIF(json:"getFullName"::text(200) , '')             as full_name
+    , NULLIF(json:"clientType"::text(200) , '')              as client_type
+    , NULLIF(json:"phoneNumber"::text(200) , '')             as phone_number
+    , NULLIF(json:"emailId"::text(200) , '')                 as email_id
+    , NULLIF(json:"accountNumber"::text(200) , '')           as account_number
+    , NULLIF(json:"originatingLocationName"::text(200) , '') as originating_location
+    , NULLIF(json:"active"::text(200) , '')                  as active
+    , NULLIF(json:"groupName"::text(200) , '')               as group_name
+    , NULLIF(json:"groupNumber"::text(200) , '')             as group_number
+    , NULLIF(json:"primaryTask"::text(200) , '')             as primary_task
+    -- period end date is left as a string, date is not complete - no year
+    , NULLIF(json:"periodEndDate"::text(200) , '')           as period_end_date
+    , _created_at                                            as _created_at
 from
     {{ source('xcm', 'client_detail') }}
--- models/base/xcm/xcm__stg_task_detail.sql
+++ models/base/xcm/xcm__stg_task_detail.sql
@@ -1,64 +1,74 @@
 select
-    _pk
-    , json:"description"::string                                as description
-    , json:"taskId"::string                                     as task_id
-    , json:"clientName"::string                                 as client_name
-    , json:"clientId"::string                                   as client_id
-    , json:"taskTypeCode"::string                               as task_type_code
-    , json:"periodEndDate"::string                              as period_end_date
-    , json:"priority"::string                                   as priority
-    , json:"taskDescription"::string                            as task_description
-    , json:"originatingLocationName"::string                    as originating_location_name
-    , json:"assignedTo"::string                                 as assigned_to
-    , json:"assignedToEmailID"::string                          as assigned_to_email_id
-    , json:"auditPartner"::string                               as audit_partner
-    , json:"auditPartnerEmailID"::string                        as audit_partner_email_id
-    , json:"auditManager"::string                               as audit_manager
-    , json:"auditManagerEmailID"::string                        as audit_manager_email_id
-    , json:"taxPartner"::string                                 as tax_partner
-    , json:"taxPartnerEmailID"::string                          as tax_partner_email_id
-    , json:"manager"::string                                    as manager
-    , json:"managerEmailID"::string                             as manager_email_id
-    , json:"responsiblePerson"::string                          as responsible_person
-    , json:"responsiblePersonEmailId"::string                   as responsible_person_email_id
-    , json:"taxSenior"::string                                  as tax_senior
-    , json:"taxSeniorEmailID"::string                           as tax_senior_email_id
-    , json:"auditSenior"::string                                as audit_senior
-    , json:"auditSeniorEmailID"::string                         as audit_senior_email_id
-    , json:"auditStaff"::string                                 as audit_staff
-    , json:"auditStaffEmailID"::string                          as audit_staff_email_id
-    , json:"taxStaff"::string                                   as tax_staff
-    , json:"taxStaffEmailID"::string                            as tax_staff_email_id
-    , json:"accountNumber"::string                              as account_number
-    , json:"primaryTaskType"::string                            as primary_task_type
-    , json:"software"::string                                   as software
-    , json:"group_Name"::string                                 as group_name
-    , json:"group_Number"::string                               as group_number
-    , json:"branch_Name"::string                                as branch_name
-    , json:"lastExtensionFiled_By"::string                      as last_extension_filed_by
-    , json:"status_Type"::string                                as status_type
-    , json:"pcaoB_or_NonPublic"::string                         as pcaob_or_non_public
-    , json:"levelOf_Service"::string                            as level_of_service
-    , json:"descriptionOfOther_Service"::string                 as description_of_other_service
-    , json:"entity_Structure"::string                           as entity_structure
-    , json:"financialStatement_Date"::string                    as financial_statement_date
-    , json:"basisof_Accounting"::string                         as basis_of_accounting
-    , json:"task_Industry"::string                              as task_industry
-    , json:"descriptionOfOther_Industry"::string                as description_of_other_industry
-    , json:"engagement_Status"::string                          as engagement_status
-    , json:"due_Date"::string                                   as due_date
-    , json:"externalId"::string                                 as external_id
-    , json:"taskCategoryCode"::string                           as task_category_code
-    , json:"taskCategoryName"::string                           as task_category_name
-    , json:"taskType"::string                                   as task_type
-    , json:"returnId"::string                                   as return_id
-    , json:"customFields"::string                               as custom_fields_parse
-    , parse_json(custom_fields_parse):"Advisor Name"::string    as advisor_name
-    , parse_json(custom_fields_parse):"Office Location"::string as location_name
-    , parse_json(custom_fields_parse):"Region"::string          as region
-    , parse_json(custom_fields_parse):"Billed Fee"::string      as billed_fee
-    , parse_json(custom_fields_parse):"Allocated Fee"::string   as allocated_fee
-    , parse_json(custom_fields_parse):"Invoice Sent"::string    as invoice_sent
-    , _created_at
+    _pk::int                                                                  as _pk
+    , NULLIF(json:"description"::text(200) , '')                              as description
+    , json:"taskId"::int                                                      as task_id
+    , NULLIF(json:"clientName"::text(200) , '')                               as client_name
+    , json:"clientId"::int                                                    as client_id
+    , NULLIF(json:"taskTypeCode"::text(200) , '')                             as task_type_code
+    , TO_DATE((json:"periodEndDate"::text(200)) , 'DD/MM/YYYY')               as period_end_date
+    , NULLIF(json:"priority"::text(200) , '')                                 as priority
+    , NULLIF(json:"taskDescription"::text(200) , '')                          as task_description
+    , NULLIF(json:"originatingLocationName"::text(200) , '')                  as originating_location_name
+    , NULLIF(json:"assignedTo"::text(200) , '')                               as assigned_to
+    , NULLIF(json:"assignedToEmailID"::text(200) , '')                        as assigned_to_email_id
+    , NULLIF(json:"auditPartner"::text(200) , '')                             as audit_partner
+    , NULLIF(json:"auditPartnerEmailID"::text(200) , '')                      as audit_partner_email_id
+    , NULLIF(json:"auditManager"::text(200) , '')                             as audit_manager
+    , NULLIF(json:"auditManagerEmailID"::text(200) , '')                      as audit_manager_email_id
+    , NULLIF(json:"taxPartner"::text(200) , '')                               as tax_partner
+    , NULLIF(json:"taxPartnerEmailID"::text(200) , '')                        as tax_partner_email_id
+    , NULLIF(json:"manager"::text(200) , '')                                  as manager
+    , NULLIF(json:"managerEmailID"::text(200) , '')                           as manager_email_id
+    , NULLIF(json:"responsiblePerson"::text(200) , '')                        as responsible_person
+    , NULLIF(json:"responsiblePersonEmailId"::text(200) , '')                 as responsible_person_email_id
+    , NULLIF(json:"taxSenior"::text(200) , '')                                as tax_senior
+    , NULLIF(json:"taxSeniorEmailID"::text(200) , '')                         as tax_senior_email_id
+    , NULLIF(json:"auditSenior"::text(200) , '')                              as audit_senior
+    , NULLIF(json:"auditSeniorEmailID"::text(200) , '')                       as audit_senior_email_id
+    , NULLIF(json:"auditStaff"::text(200) , '')                               as audit_staff
+    , NULLIF(json:"auditStaffEmailID"::text(200) , '')                        as audit_staff_email_id
+    , NULLIF(json:"taxStaff"::text(200) , '')                                 as tax_staff
+    , NULLIF(json:"taxStaffEmailID"::text(200) , '')                          as tax_staff_email_id
+    , NULLIF(json:"accountNumber"::text(200) , '')                            as account_number
+    , NULLIF(json:"primaryTaskType"::text(200) , '')                          as primary_task_type
+    , NULLIF(json:"software"::text(200) , '')                                 as software
+    , NULLIF(json:"group_Name"::text(200) , '')                               as group_name
+    , NULLIF(json:"group_Number"::text(200) , '')                             as group_number
+    , NULLIF(json:"branch_Name"::text(200) , '')                              as branch_name
+    , NULLIF(json:"lastExtensionFiled_By"::text(200) , '')                    as last_extension_filed_by
+    , NULLIF(json:"status_Type"::text(200) , '')                              as status_type
+    , NULLIF(json:"pcaoB_or_NonPublic"::text(200) , '')                       as pcaob_or_non_public
+    , NULLIF(json:"levelOf_Service"::text(200) , '')                          as level_of_service
+    , NULLIF(json:"descriptionOfOther_Service"::text(200) , '')               as description_of_other_service
+    , NULLIF(json:"entity_Structure"::text(200) , '')                         as entity_structure
+    , TRY_TO_DATE((json:"financialStatement_Date"::text(200)) , 'DD/MM/YYYY') as financial_statement_date
+    , NULLIF(json:"basisof_Accounting"::text(200) , '')                       as basis_of_accounting
+    , NULLIF(json:"task_Industry"::text(200) , '')                            as task_industry
+    , NULLIF(json:"descriptionOfOther_Industry"::text(200) , '')              as description_of_other_industry
+    , NULLIF(json:"engagement_Status"::text(200) , '')                        as engagement_status
+    , TRY_TO_DATE((json:"due_Date"::text(200)) , 'DD/MM/YYYY')                as due_date
+    , NULLIF(json:"externalId"::text(200) , '')                               as external_id
+    , NULLIF(json:"taskCategoryCode"::text(200) , '')                         as task_category_code
+    , NULLIF(json:"taskCategoryName"::text(200) , '')                         as task_category_name
+    , NULLIF(json:"taskType"::text(200) , '')                                 as task_type
+    , NULLIF(json:"returnId"::text(200) , '')                                 as return_id
+    , NULLIF(json:"customFields"::text(200) , '')                             as custom_fields
+    , NULLIF(PARSE_JSON(custom_fields):"Advisor Name"::text(200) , '')        as advisor_name
+    , NULLIF(PARSE_JSON(custom_fields):"Office Location"::text(200) , '')     as location_name
+    , NULLIF(PARSE_JSON(custom_fields):"Region"::text(200) , '')              as region
+    , TRY_CAST((
+        REPLACE((
+            PARSE_JSON(custom_fields):"Billed Fee"
+        ) , '$' , '')
+    ) as decimal(18 , 2))                                                     as billed_fee
+    , TRY_CAST((
+        REPLACE((
+            PARSE_JSON(custom_fields):"Allocated Fee"
+        ) , '$' , '')
+    ) as decimal(18 , 2))                                                     as allocated_fee
+    , TRY_TO_DATE(
+        (PARSE_JSON(custom_fields):"Invoice Sent"::text(200)) , 'MM/DD/YYYY'
+    )                                                                         as invoice_sent
+    , _created_at                                                             as _create_at
 from
     {{ source('xcm', 'task_detail') }}
