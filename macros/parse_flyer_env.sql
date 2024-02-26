d7e05d2ae9807cb197b08b0afedef6aac82ce3c5
Grant Kilber <116096178+grantith@users.noreply.github.com>
2024-02-26 11:43:54 -0600

    refactor: [Flyer] Replaces old prod with new prod (_env) (#220)

--- macros/parse_flyer_env.sql
+++ macros/parse_flyer_env.sql
@@ -1,11 +1,13 @@
 {% macro parse_flyer_env(col='_uri') -%}
 case
-    when {{ col }} ilike '%ec2-3-89-126-76%'
-        then 'dev'
+    when {{ col }} ilike '%mariner.flyerapps.net%'
+        then 'prod'
+    when {{ col }} ilike '%copilot-2.flyerapps.net%'
+        then 'prod-old'
     when {{ col }} ilike '%ec2-44-201-187-118%'
         then 'uat'
-    when {{ col }} ilike '%copilot-2.flyerapps.net%'
-        then 'prod'
+    when {{ col }} ilike '%ec2-3-89-126-76%'
+        then 'dev'
     else 'unknown'
     end::text(200) as _env
 {%- endmacro %}
