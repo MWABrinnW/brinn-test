3fe5da2c4b438dd076614b7ca9d980cb1cb87dd0
Spencer Brown <161622453+ospenbro@users.noreply.github.com>
2024-06-03 14:26:17 -0500

    feat: [Salesforce Baystate] adds advisor_split_code_c and advisor_split_detail_c (#328)

---
+++ models/base/salesforce_baystate/salesforce_baystate__base_advisor_split_code_c.sql
@@ -0,0 +1,24 @@
+select
+    'salesforce'::text(200)                                   as system_name
+    , 'baystate'::text(200)                                   as system_instance
+    , concat(system_name , '__' , system_instance)::text(200) as system_key
+    , 'baystate'::text(200)                                   as firm_source
+    , json:_FIVETRAN_SYNCED::timestampntz                     as _fivetran_synced
+    , json:CREATED_DATE::timestampntz                         as created_date
+    , json:CREATED_BY_ID::text(200)                           as created_by_id
+    , json:ID::text(200)                                      as id
+    , json:IS_DELETED::boolean                                as is_deleted
+    , json:ADVISOR_NAME_C::text(200)                          as advisor_name_c
+    , json:ADVISOR_SPLIT_CODE_C::text(200)                    as advisor_split_code_c
+    , json:LAST_MODIFIED_BY_ID::text(200)                     as last_modified_by_id
+    , json:LAST_MODIFIED_DATE::timestampntz                   as last_modified_date
+    , nullif(json:LAST_VIEWED_DATE , '')::timestampntz        as last_viewed_date
+    , json:PERCENTAGE_C::dec(20 , 2)                          as percentage_c
+    , json:SYSTEM_MODSTAMP::timestampntz                      as system_modstamp
+    , nullif(json:LAST_REFERENCED_DATE , '')::timestampntz    as last_referenced_date
+    , json:NAME::text(200)                                    as name--noqa: RF04
+    , json:_FIVETRAN_DELETED::boolean                         as _fivetran_deleted
+    , effective_at::timestampntz                              as effective_at
+    , _created_at::timestampntz                               as _created_at
+
+from {{ source('salesforce_baystate', 'advisor_split_detail_c') }}
---
+++ models/base/salesforce_baystate/salesforce_baystate__base_advisors_split_detail_c.sql
@@ -0,0 +1,23 @@
+select
+    'salesforce'::text(200)                                   as system_name
+    , 'baystate'::text(200)                                   as system_instance
+    , concat(system_name , '__' , system_instance)::text(200) as system_key
+    , 'baystate'::text(200)                                   as firm_source
+    , json:_FIVETRAN_SYNCED::timestampntz                     as _fivetran_synced
+    , json:CREATED_DATE::timestampntz                         as created_date
+    , json:CREATED_BY_ID::text(200)                           as created_by_id
+    , json:ID::text(200)                                      as id
+    , json:IS_DELETED::boolean                                as is_deleted
+    , json:TOTAL_MARKET_VALUE_ROLLUP_C::dec(20 , 2)           as total_market_value_rollup_c
+    , json:LAST_MODIFIED_BY_ID::text(200)                     as last_modified_by_id
+    , json:LAST_MODIFIED_DATE::timestampntz                   as last_modified_date
+    , json:OWNER_ID::text(200)                                as owner_id
+    , nullif(json:LAST_VIEWED_DATE , '')::timestampntz        as last_viewed_date
+    , json:SYSTEM_MODSTAMP::timestampntz                      as system_modstamp
+    , nullif(json:LAST_REFERENCED_DATE , '')::timestampntz    as last_referenced_date
+    , json:NAME::text(200)                                    as name--noqa: RF04
+    , json:_FIVETRAN_DELETED::boolean                         as _fivetran_deleted
+    , effective_at::timestampntz                              as effective_at
+    , _created_at::timestampntz                               as _created_at
+
+from {{ source('salesforce_baystate', 'advisor_split_code_c') }}
