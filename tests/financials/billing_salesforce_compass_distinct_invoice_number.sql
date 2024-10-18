e684b3b33a0a7923883f25acf4421916c93262ba
Andrew Charlton <143548142+acharchar@users.noreply.github.com>
2024-10-18 14:16:49 -0500

    feat: [Billing Wealth] tests for distinct invoice numbers (#412)
    * [tests:financials] adds test for distinct salesforce compass invoice numbers
    
    * [tests:financials] linter
    
    * [singular tests: finanicals] error if BRs are duplicated
    
    * [singular tests: finanicals] lints

---
+++ tests/financials/billing_salesforce_compass_distinct_invoice_number.sql
@@ -0,0 +1,12 @@
+{{ config(
+    severity='error',
+) }}
+
+select distinct--noqa: AM01
+    invoice_number_source
+    , count(*) as cnt
+from {{ ref('bld_billing_wealth') }}
+where true
+    and system_key = 'salesforce__compass'
+group by all
+having cnt > 1
