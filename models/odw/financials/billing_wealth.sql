44155a57cbdb8cd5334e844782fa1f7ddb70a8fc
Andrew Charlton <143548142+acharchar@users.noreply.github.com>
2024-10-25 08:10:58 -0500

    fix: [Billing Advisory] corrects filter in the "billing_wealth" view (#462)

--- models/odw/financials/billing_wealth.sql
+++ models/odw/financials/billing_wealth.sql
@@ -1,16 +1,20 @@
-select
-    * exclude (
-        fee_schedule , fee_schedule_type , _extra_fields
-    )
-    , 0::int as is_legacy
-from {{ ref('bld_billing_wealth_like') }}
-union all
-select
-    * exclude (
-        fee_schedule , fee_schedule_type , _extra_fields
-    )
-    , 1::int as is_legacy
-from {{ ref('stg_bills_legacy_wealth') }}
+with cte_union as (
+    select
+        * exclude (
+            fee_schedule , fee_schedule_type , _extra_fields
+        )
+        , 0::int as is_legacy
+    from {{ ref('bld_billing_wealth_like') }}
+    union all
+    select
+        * exclude (
+            fee_schedule , fee_schedule_type , _extra_fields
+        )
+        , 1::int as is_legacy
+    from {{ ref('stg_bills_legacy_wealth') }}
+)
+
+select * from cte_union
 where true
     and system_key in (
         'addepar__corbenic'
@@ -20,3 +24,4 @@ where true
         , 'sei__manasquan'
         , 'envestnet__manasquan'
     )
+    or is_legacy = 1
