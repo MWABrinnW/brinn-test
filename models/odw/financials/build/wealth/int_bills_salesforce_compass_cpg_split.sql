5ed96ab9522e3848302d17b29f638c6bcdebbd77
Andrew Charlton <143548142+acharchar@users.noreply.github.com>
2024-11-04 16:17:45 -0600

    fix: [Billing Advisory] fixes inconsistent join to account number (#468)
    * [int bills salesforce compass cpg split] fixes incorrect join to account number
    
    * [int bills salesforce compass cpg split] linter

--- models/odw/financials/build/wealth/int_bills_salesforce_compass_cpg_split.sql
+++ models/odw/financials/build/wealth/int_bills_salesforce_compass_cpg_split.sql
@@ -2,14 +2,16 @@
 select
     ir.*
     , case
-        when (ir.account_number_c = replace(cpg_ba.account_number , '-' , '') and ir.orion_bill_id_c is null) then 1
+        when
+            (replace(ir.account_number_c , '-' , '') = replace(cpg_ba.account_number , '-' , '') and ir.orion_bill_id_c is null)
+            then 1
         else 0
     end::int as is_cpg
 from {{ ref('salesforce_compass__base_invoice_review_c') }} as ir
 inner join edw.ref.dates as dt
     on ir.invoice_date_c = dt.date_key
 left join {{ ref('tamarac_state_college_history__base_accounts') }} as cpg_ba
-    on ir.account_number_c = replace(cpg_ba.account_number , '-' , '')
+    on replace(ir.account_number_c , '-' , '') = replace(cpg_ba.account_number , '-' , '')
     and
     case
         when (ir.invoice_date_c = cpg_ba.effective_date or dt.prior_market_date = cpg_ba.effective_date)
