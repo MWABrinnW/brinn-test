1bf6101d6cd71f82b4f561bccc56ca6a88f29153
Allen Jones <122400605+allen-mwa@users.noreply.github.com>
2025-01-08 15:54:03 -0600

    fix: [Rev360] Repair salesforce_compass test failure (#532)

--- models/odw/financials/build/wealth/nml_bills_salesforce_compass_cpg.sql
+++ models/odw/financials/build/wealth/nml_bills_salesforce_compass_cpg.sql
@@ -11,7 +11,7 @@ with dynamics_finance_accounts_cte as (
                     , created_at desc
             )
             as rn
-    from datalake.dynamics_tamarac_cpg.stg_finance_accounts
+    from {{ ref('dynamics_tamarac_cpg__stg_finance_accounts') }}
     where date(effective_at) >= '1900-01-01'
         and is_head_for_day = 1
     --and dy_acct.TAM_UPLOAD_ID = '61507'
@@ -27,7 +27,7 @@ with dynamics_finance_accounts_cte as (
         , hh.column_name                                    as split_dtl_split_index
         , to_number(right(trim(split_dtl_split_index) , 1)) as split_dtl_sol_role_id
         , quantity                                          as split_dtl_split_amount
-    from datalake.dynamics_tamarac_cpg.stg_accounts as hh
+    from {{ ref('dynamics_tamarac_cpg__stg_accounts') }} as hh
     unpivot (quantity for column_name in (tamc_split_1 , tamc_split_2 , tamc_split_3 , tamc_split_4))
     where true
         and is_head_for_day = 1
@@ -47,30 +47,30 @@ with dynamics_finance_accounts_cte as (
 
     -- , cte_1.SPLIT_DTL_SOL_ROLE_ID
     -- , cte_1.SPLIT_DTL_ACCOUNT_ID
-    from datalake.dynamics_tamarac_cpg.stg_connections as ctn_1
+    from {{ ref('dynamics_tamarac_cpg__stg_connections') }} as ctn_1
 
-    left join datalake.dynamics_tamarac_cpg.stg_connection_roles as ctr_1
+    left join {{ ref('dynamics_tamarac_cpg__stg_connection_roles') }} as ctr_1
         on ctn_1._record_1_role_id_value = ctr_1.connection_role_id
         and date(ctn_1.effective_at) = date(ctr_1.effective_at)
         and ctr_1.is_head_for_day = 1
 
-    inner join datalake.dynamics_tamarac_cpg.stg_connections as ctn_2
+    inner join {{ ref('dynamics_tamarac_cpg__stg_connections') }} as ctn_2
         on ctn_1._related_connection_id_value = ctn_2.connection_id
         and date(ctn_1.effective_at) = date(ctn_2.effective_at)
         and ctn_2.is_head_for_day = 1
 
-    left join datalake.dynamics_tamarac_cpg.stg_connection_roles as ctr_2
+    left join {{ ref('dynamics_tamarac_cpg__stg_connection_roles') }} as ctr_2
         on ctn_2._record_1_role_id_value = ctr_2.connection_role_id
         and date(ctn_1.effective_at) = date(ctr_2.effective_at)
         and ctr_2.is_head_for_day = 1
 
-    inner join datalake.dynamics_tamarac_cpg.stg_accounts as cpg_acc
+    inner join {{ ref('dynamics_tamarac_cpg__stg_accounts') }} as cpg_acc
         on ctn_1._record_2_id_value = cpg_acc.account_id
         and date(ctn_1.effective_at) = date(cpg_acc.effective_at)
         and cpg_acc.is_head_for_day = 1
 
 
-    inner join datalake.dynamics_tamarac_cpg.stg_account_types as cpg_act
+    inner join {{ ref('dynamics_tamarac_cpg__stg_account_types') }} as cpg_act
         on cpg_acc._account_type_id_value = cpg_act.tam_account_type_id
         and date(ctn_1.effective_at) = date(cpg_act.effective_at)
         and cpg_act.is_head_for_day = 1
@@ -358,7 +358,7 @@ left join edw.ref.dates as dt
 -- This is because over time the account record can change (i.e. advisor assignment).
 -- -- We want to know what it looked like at the time of billing.
 inner join {{ ref('tamarac_state_college_history__base_accounts') }} as ba--GWH: ensure only Tamarac CPG Accounts are included
-    on ir.account_number_c = ba.account_number
+    on replace(ir.account_number_c , '-' , '') = replace(ba.account_number , '-' , '')
     and ir.invoice_date_c = ba.effective_date
     and ba.rn = 1
     and ba.entity_type in ('Single Account')
--- tests/financials/billing_monthly_client_net_equality_to_source.sql
+++ tests/financials/billing_monthly_client_net_equality_to_source.sql
@@ -4,64 +4,76 @@
 
 {%- set base_bills = [
     {'model': 'salesforce_compass__base_invoice_review_c', 'invoice_date': 'invoice_date_c',
-     'client_fee_net': 'net_fee_c', 'is_head': 'yes'},
+     'client_fee_net': 'net_fee_c', 'is_head': 'yes', 'created_at': "greatest(last_modified_date,created_date)"},
     {'model': 'addepar_corbenic_history__base_bills', 'invoice_date': 'billing_date',
-     'client_fee_net': 'billing_fee_value', 'is_head': 'yes'},
+     'client_fee_net': 'billing_fee_value', 'is_head': 'yes', 'created_at': '_created_at'},
     {'model': 'black_diamond_baystate__base_bills', 'invoice_date': "(dateadd('day', -1, date_trunc('quarter', cash_available_date)))::date",
-     'client_fee_net': 'total_period_fee', 'is_head': 'yes'},
+     'client_fee_net': 'total_period_fee', 'is_head': 'yes', 'created_at': '_created_at'},
     {'model': 'black_diamond_houston__base_bills', 'invoice_date': "(dateadd('day', -1, date_trunc('quarter', cash_available_date)))::date",
-     'client_fee_net': 'total_period_fee', 'is_head': 'yes'},
+     'client_fee_net': 'total_period_fee', 'is_head': 'yes', 'created_at': '_created_at'},
     {'model': 'black_diamond_mps__base_bills', 'invoice_date': "(dateadd('day', -1, date_trunc('quarter', cash_available_date)))::date",
-     'client_fee_net': 'total_period_fee', 'is_head': 'yes'},
+     'client_fee_net': 'total_period_fee', 'is_head': 'yes', 'created_at': '_created_at'},
     {'model': 'black_diamond_uhnw__base_bills', 'invoice_date': "(dateadd('day', -1, date_trunc('quarter', cash_available_date)))::date",
-     'client_fee_net': 'total_period_fee', 'is_head': 'yes'},
+     'client_fee_net': 'total_period_fee', 'is_head': 'yes', 'created_at': '_created_at'},
     {'model': 'envestnet_manasquan__stg_bills', 'invoice_date': 'invoice_date',
-     'client_fee_net': 'total_fee_amount', 'is_head': 'yes'},
+     'client_fee_net': 'total_fee_amount', 'is_head': 'yes', 'created_at': '_created_at'},
     {'model': 'sei_manasquan__base_bills', 'invoice_date': 'fee_effective_date',
-     'client_fee_net': 'fees_collected', 'is_head': 'yes'}
+     'client_fee_net': 'fees_collected', 'is_head': 'yes', 'created_at': '_created_at'}
 ] %}
 
-with cte_base as (
-    {% for source in base_bills %}
+with cte_billing_max as (
+    select
+        system_key                   as billing_system_key
+        , max(_created_at)::datetime as max_created_at
+    from
+        {{ ref('bld_billing_wealth') }}
+    group by all
+)
+
+, cte_base as (
+    {%- for source in base_bills %}
         select
-            system_key                                             as base_system_key--noqa: LT01
+            a.system_key                                             as base_system_key--noqa: LT01
             , last_day(date_trunc('month' , {{ source['invoice_date'] }}))::date as base_invoice_date
             , sum({{ source['client_fee_net'] }})::number(15 , 2)                       as base_client_fee_net--noqa: LT01
         from
-            {{ ref(source['model']) }}
+            {{ ref(source['model']) }} as a
+        left join cte_billing_max as b
+            on a.system_key = b.billing_system_key
         where
             true
             and last_day(date_trunc('month' , {{ source['invoice_date'] }}::date))
             >= last_day(dateadd('month' , -15 , date_trunc('month' , current_date)))
             and last_day(date_trunc('month' , {{ source['invoice_date'] }}::date))
             <= last_day(dateadd('month' , -1 , date_trunc('month' , current_date)))
+            and {{ source['created_at'] }} <= b.max_created_at
 
             -- all models
-            {% if source['is_head'] == 'yes' %}
-                and is_head = 1
-            {% endif %}
+            {%- if source['is_head'] == 'yes' %}
+                and a.is_head = 1
+            {%- endif %}
 
             -- salesforce only
-            {% if source['model'] == 'salesforce_compass__base_invoice_review_c' %}
-                and is_latest = 1
-                and is_deleted = 0
-                and _fivetran_deleted = 0
-            {% endif %}
+            {%- if source['model'] == 'salesforce_compass__base_invoice_review_c' %}
+                and a.is_latest = 1
+                and a.is_deleted = 0
+                and a._fivetran_deleted = 0
+            {%- endif %}
 
         group by all
 
         {%- if not loop.last %}
             union all
-        {% endif %}
+        {%- endif %}
 
     {% endfor %}
 )
 
-, cte_billings as (
+, cte_billing as (
     select
-        system_key                                                 as billings_system_key
-        , last_day(date_trunc('month' , invoice_date::date))::date as billings_invoice_date
-        , sum(client_fee_net)::number(15 , 2)                      as billings_client_fee_net
+        system_key                                                 as billing_system_key
+        , last_day(date_trunc('month' , invoice_date::date))::date as billing_invoice_date
+        , sum(client_fee_net)::number(15 , 2)                      as billing_client_fee_net
     from
         {{ ref('bld_billing_wealth') }}
     where
@@ -75,12 +87,12 @@ with cte_base as (
 
 select
     base.*
-    , billings.*
-    , (base.base_client_fee_net - billings.billings_client_fee_net)::number(15 , 2) as revenue_diff
-    , (base.base_client_fee_net = billings.billings_client_fee_net)::boolean        as revenue_equality
+    , billing.*
+    , (base.base_client_fee_net - billing.billing_client_fee_net)::number(15 , 2) as revenue_diff
+    , (base.base_client_fee_net = billing.billing_client_fee_net)::boolean        as revenue_equality
 from cte_base as base
-left join cte_billings as billings
-    on base.base_system_key = billings.billings_system_key
-    and base.base_invoice_date = billings.billings_invoice_date
+left join cte_billing as billing
+    on base.base_system_key = billing.billing_system_key
+    and base.base_invoice_date = billing.billing_invoice_date
 where revenue_equality = false
 order by base.base_system_key asc , base.base_invoice_date desc
