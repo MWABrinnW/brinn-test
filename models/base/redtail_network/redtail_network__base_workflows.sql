e36853eb8d673376a16bd71e4b18a4a4af95498a
Allen Jones <122400605+allen-mwa@users.noreply.github.com>
2025-01-15 08:41:29 -0600

    fix: [Redtail] workflows deduping (#549)

--- models/base/redtail_network/redtail_network__base_workflows.sql
+++ models/base/redtail_network/redtail_network__base_workflows.sql
@@ -2,91 +2,111 @@
 
 with workflow_steps_cte as (
     select
-        s.json:id::int                                     as workflow_id
-        , workflow_steps.value:id::int                     as step_id
-        , workflow_steps.value:name::varchar               as step_name
-        , workflow_steps.value:workflow_id::varchar        as step_workflow_id
-        , workflow_steps.value:order_position::int         as step_order_position
-        , workflow_steps.value:interval::varchar           as step_interval
-        , workflow_steps.value:frequency::varchar          as step_frequency
-        , workflow_steps.value:interval_placement::varchar as step_interval_placement
-        , workflow_steps.value:display_interval::varchar   as step_display_interval
-        , workflow_steps.value:completion_date::timestamp  as step_completion_date
-        , workflow_steps.value:description::varchar        as step_description
-        , workflow_steps.value:comments::varchar           as step_comments
-        , workflow_steps.value:tasks_remaining::int        as step_tasks_remaining
-        , workflow_steps.value:deleted::boolean            as step_deleted
-        , workflow_steps.value:created_at::timestamp       as step_created_at
-        , workflow_steps.value:updated_at::timestamp       as step_updated_at
-        , workflow_steps.value:workflow_step_tasks         as workflow_step_tasks
-    from {{ src }} as s
-    , lateral flatten(input => s.json:workflow_steps) as workflow_steps
+        w.json:id::int                         as workflow_id
+        , ws.value:id::int                     as step_id
+        , ws.value:name::varchar               as step_name
+        , ws.value:workflow_id::varchar        as step_workflow_id
+        , ws.value:order_position::int         as step_order_position
+        , ws.value:interval::varchar           as step_interval
+        , ws.value:frequency::varchar          as step_frequency
+        , ws.value:interval_placement::varchar as step_interval_placement
+        , ws.value:display_interval::varchar   as step_display_interval
+        , ws.value:completion_date::timestamp  as step_completion_date
+        , ws.value:description::varchar        as step_description
+        , ws.value:comments::varchar           as step_comments
+        , ws.value:tasks_remaining::int        as step_tasks_remaining
+        , ws.value:deleted::int                as step_deleted
+        , ws.value:created_at::timestamp       as step_created_at
+        , ws.value:updated_at::timestamp       as step_updated_at
+        , ws.value:workflow_step_tasks         as workflow_step_tasks
+        , ws.value:workflow_step_outcomes      as workflow_step_outcomes
+        , w._created_at::timestamp             as _created_at
+    from {{ src }} as w
+    , lateral flatten(input => w.json:workflow_steps) as ws
 )
 
 , workflow_step_tasks_cte as (
     select
-        workflow_steps_cte.workflow_id                             as workflow_id
-        , workflow_steps_cte.step_id                               as step_id
-        , workflow_step_tasks.value:id::varchar                    as task_id
-        , workflow_step_tasks.value:name::varchar                  as task_name
-        , workflow_step_tasks.value:workflow_step_id::int          as task_workflow_step_id
-        , workflow_step_tasks.value:parent_step_task_id::varchar   as parent_step_task_id
-        , workflow_step_tasks.value:assignee_id::int               as task_assignee_id
-        , workflow_step_tasks.value:assignee_type::int             as task_assignee_type
-        , workflow_step_tasks.value:display_position::int          as task_display_position
-        , workflow_step_tasks.value:due_date::timestamp            as task_due_date
-        , workflow_step_tasks.value:calculated_due_date::timestamp as task_calculated_due_date
-        , workflow_step_tasks.value:completion_date::timestamp     as task_completion_date
-        , workflow_step_tasks.value:description::varchar           as task_description
-        , workflow_step_tasks.value:deleted::boolean               as task_deleted
-        , workflow_step_tasks.value:created_at::timestamp          as task_created_at
-        , workflow_step_tasks.value:updated_at::timestamp          as task_updated_at
-    from workflow_steps_cte
-    , lateral flatten(input => workflow_steps_cte.workflow_step_tasks) as workflow_step_tasks
+        ws.workflow_id                             as workflow_id
+        , ws.step_id                               as step_id
+        , wst.value:id::varchar                    as task_id
+        , wst.value:name::varchar                  as task_name
+        , wst.value:workflow_step_id::int          as task_workflow_step_id
+        , wst.value:parent_step_task_id::varchar   as parent_step_task_id
+        , wst.value:assignee_id::int               as task_assignee_id
+        , wst.value:assignee_type::int             as task_assignee_type
+        , wst.value:display_position::int          as task_display_position
+        , wst.value:due_date::timestamp            as task_due_date
+        , wst.value:calculated_due_date::timestamp as task_calculated_due_date
+        , wst.value:completion_date::timestamp     as task_completion_date
+        , wst.value:description::varchar           as task_description
+        , wst.value:deleted::int                   as task_deleted
+        , wst.value:created_at::timestamp          as task_created_at
+        , wst.value:updated_at::timestamp          as task_updated_at
+        , ws._created_at::timestamp                as _created_at
+    from workflow_steps_cte as ws
+    , lateral flatten(input => ws.workflow_step_tasks) as wst
+)
+
+, workflow_step_outcomes_cte as (
+    select
+        ws.workflow_id                         as workflow_id
+        , ws.step_id                           as step_id
+        , wso.value:id::int                    as outcome_id
+        , wso.value:name::varchar              as outcome_name
+        , wso.value:workflow_step_id::int      as outcome_workflow_step_id
+        , wso.value:next_workflow_step_id::int as outcome_next_workflow_step_id
+        , wso.value:deleted::int               as outcome_deleted
+        , wso.value:created_at::timestamp      as outcome_created_at
+        , wso.value:updated_at::timestamp      as outcome_updated_at
+        , ws._created_at::timestamp            as _created_at
+    from workflow_steps_cte as ws
+    , lateral flatten(input => ws.workflow_step_outcomes) as wso
 )
 
 , linked_contacts_cte as (
     select
-        s.json:id::int                                as workflow_id
-        , linked_contacts.value:type::varchar         as contact_type
-        , linked_contacts.value:first_name::varchar   as contact_first_name
-        , linked_contacts.value:middle_name::varchar  as contact_middle_name
-        , linked_contacts.value:last_name::varchar    as contact_last_name
-        , linked_contacts.value:company_name::varchar as contact_company_name
-        , linked_contacts.value:full_name::varchar    as contact_full_name
-        , linked_contacts.value:nickname::varchar     as contact_nickname
-        , linked_contacts.value:suffix_id::varchar    as contact_suffix_id
-        , linked_contacts.value:suffix::varchar       as contact_suffix
-        , linked_contacts.value:job_title::varchar    as contact_job_title
-        , linked_contacts.value:favorite::boolean     as contact_favorite
-        , linked_contacts.value:pronouns::varchar     as contact_pronouns
-        , linked_contacts.value:deleted::boolean      as contact_deleted
-        , linked_contacts.value:created_at::timestamp as contact_created_at
-        , linked_contacts.value:updated_at::timestamp as contact_updated_at
-        , linked_contacts.value:contact_id::int       as contact_id
-    from {{ src }} as s
-    , lateral flatten(input => s.json:linked_contacts) as linked_contacts
+        w.json:id::int                   as workflow_id
+        , lc.value:type::varchar         as contact_type
+        , lc.value:first_name::varchar   as contact_first_name
+        , lc.value:middle_name::varchar  as contact_middle_name
+        , lc.value:last_name::varchar    as contact_last_name
+        , lc.value:company_name::varchar as contact_company_name
+        , lc.value:full_name::varchar    as contact_full_name
+        , lc.value:nickname::varchar     as contact_nickname
+        , lc.value:suffix_id::varchar    as contact_suffix_id
+        , lc.value:suffix::varchar       as contact_suffix
+        , lc.value:job_title::varchar    as contact_job_title
+        , lc.value:favorite::int         as contact_favorite
+        , lc.value:pronouns::varchar     as contact_pronouns
+        , lc.value:deleted::int          as contact_deleted
+        , lc.value:created_at::timestamp as contact_created_at
+        , lc.value:updated_at::timestamp as contact_updated_at
+        , lc.value:contact_id::int       as contact_id
+        , w._created_at                  as _created_at
+    from {{ src }} as w
+    , lateral flatten(input => w.json:linked_contacts) as lc
 )
 
 select
     -- top level fields
-    s.json:id::int                         as workflow_id
-    , s.json:workflow_template_id::varchar as workflow_template_id
-    , s.json:name::varchar                 as workflow_name
-    , s.json:due_date::timestamp           as workflow_due_date
-    , s.json:current_step_id::int          as current_step_id
-    , s.json:status::int                   as workflow_status
-    , s.json:assignee_id::int              as workflow_assignee_id
-    , s.json:assignee_type::varchar        as assignee_type
-    , s.json:completion_date::timestamp    as workflow_completion_date
-    , s.json:description::varchar          as workflow_description
-    , s.json:category_id::int              as category_id
-    , s.json:category::varchar             as workflow_category
-    , s.json:exclude_weekends::boolean     as exclude_weekends
-    , s.json:deleted::boolean              as workflow_deleted
-    , s.json:created_at::timestamp         as workflow_created_at
-    , s.json:updated_at::timestamp         as workflow_updated_at
-    , s.json:_effective_at::timestamp      as workflow_effective_at
+    w.json:id::int                         as workflow_id
+    , w.json:workflow_template_id::varchar as workflow_template_id
+    , w.json:name::varchar                 as workflow_name
+    , w.json:due_date::timestamp           as workflow_due_date
+    , w.json:current_step_id::int          as workflow_current_step_id
+    , w.json:status::int                   as workflow_status
+    , w.json:assignee_id::int              as workflow_assignee_id
+    , w.json:assignee_type::varchar        as workflow_assignee_type
+    , w.json:completion_date::timestamp    as workflow_completion_date
+    , w.json:description::varchar          as workflow_description
+    , w.json:category_id::int              as workflow_category_id
+    , w.json:category::varchar             as workflow_category
+    , w.json:exclude_weekends::int         as workflow_exclude_weekends
+    , w.json:deleted::int                  as workflow_deleted
+    , w.json:created_at::timestamp         as workflow_created_at
+    , w.json:updated_at::timestamp         as workflow_updated_at
+    , w.json:_effective_at::timestamp      as workflow_effective_at
 
     -- workflow steps
     , ws.step_id                           as step_id
@@ -106,20 +126,29 @@ select
     , ws.step_updated_at                   as step_updated_at
 
     -- workflow step tasks
-    , wt.task_id                           as task_id
-    , wt.task_name                         as task_name
-    , wt.task_workflow_step_id             as task_workflow_step_id
-    , wt.parent_step_task_id               as parent_step_task_id
-    , wt.task_assignee_id                  as task_assignee_id
-    , wt.task_assignee_type                as task_assignee_type
-    , wt.task_display_position             as task_display_position
-    , wt.task_due_date                     as task_due_date
-    , wt.task_calculated_due_date          as task_calculated_due_date
-    , wt.task_completion_date              as task_completion_date
-    , wt.task_description                  as task_description
-    , wt.task_deleted                      as task_deleted
-    , wt.task_created_at                   as task_created_at
-    , wt.task_updated_at                   as task_updated_at
+    , wst.task_id                          as task_id
+    , wst.task_name                        as task_name
+    , wst.task_workflow_step_id            as task_workflow_step_id
+    , wst.parent_step_task_id              as task_parent_step_task_id
+    , wst.task_assignee_id                 as task_assignee_id
+    , wst.task_assignee_type               as task_assignee_type
+    , wst.task_display_position            as task_display_position
+    , wst.task_due_date                    as task_due_date
+    , wst.task_calculated_due_date         as task_calculated_due_date
+    , wst.task_completion_date             as task_completion_date
+    , wst.task_description                 as task_description
+    , wst.task_deleted                     as task_deleted
+    , wst.task_created_at                  as task_created_at
+    , wst.task_updated_at                  as task_updated_at
+
+    -- workflow step outcomes
+    , wso.outcome_id                       as outcome_id
+    , wso.outcome_name                     as outcome_name
+    , wso.outcome_workflow_step_id         as outcome_workflow_step_id
+    , wso.outcome_next_workflow_step_id    as outcome_next_workflow_step_id
+    , wso.outcome_deleted                  as outcome_deleted
+    , wso.outcome_created_at               as outcome_created_at
+    , wso.outcome_updated_at               as outcome_updated_at
 
     -- linked contacts
     , lc.contact_type                      as contact_type
@@ -139,12 +168,23 @@ select
     , lc.contact_updated_at                as contact_updated_at
     , lc.contact_id                        as contact_id
 
-    , s._created_at                        as _created_at
+    , {{ col_is_head(reference=src,
+     reference_date_col='_effective_at::date', source_date_col='_effective_at::date') }}
+    , w._effective_at::timestamp_ltz       as effective_at
+    , w._effective_at::date                as effective_date
+    , w._created_at::timestamp_ltz         as _source_loaded_at
+    , w._source_file::varchar(200)         as _source_file
 
-from {{ src }} as s
+from {{ src }} as w
 left join workflow_steps_cte as ws
-    on s.json:id::int = ws.workflow_id
-left join workflow_step_tasks_cte as wt
-    on ws.step_id = wt.step_id
+    on w.json:id::int = ws.workflow_id
+    and w._created_at = ws._created_at
+left join workflow_step_tasks_cte as wst
+    on ws.step_id = wst.task_workflow_step_id
+    and ws._created_at = wst._created_at
+left join workflow_step_outcomes_cte as wso
+    on ws.step_id = wso.outcome_workflow_step_id
+    and ws._created_at = wso._created_at
 left join linked_contacts_cte as lc
-    on s.json:id::int = lc.workflow_id
+    on w.json:id::int = lc.workflow_id
+    and w._created_at = lc._created_at
