ba5ce643365ba91f05c45b665cf65d13b31b37e1
Austin Lanning <116376257+mwalanning@users.noreply.github.com>
2025-02-28 04:37:05 -0800

    fix: [Moxy] Dedupe New Securities Section of Post Import Report (#584)
    port the securities_build deduping fix to post-import report

--- models/odw/moxy/moxy__rpt_sod_post_import_new_securities.sql
+++ models/odw/moxy/moxy__rpt_sod_post_import_new_securities.sql
@@ -1,8 +1,27 @@
+with cte_securities as (
+    select
+        symbol
+        , sec_type
+        , cusip
+        , price
+        , product_name
+        , iso_cfi
+        , is_intraday_import
+        , is_new
+        -- Mitigates having multiple securities of the same type with different prices
+        , row_number() over (
+            partition by symbol , sec_type
+            order by price desc
+        ) as rn
+    from {{ ref('moxy__int_securities_build') }}
+)
+
 select
     symbol         as "Symbol"
     , sec_type     as "Type"
     , iso_cfi      as iso
     , product_name as "Name"
     , cusip        as "Cusip"
-from {{ ref('moxy__int_securities_build') }}
+from cte_securities
 where is_new = 1
+    and rn = 1
