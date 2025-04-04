28718515e18ef35b6083865987c74ca121513c29
Allen Jones <122400605+allen-mwa@users.noreply.github.com>
2025-04-04 11:29:32 -0500

    refactor: [seeds] add engineering access to all seeds (#617)

--- models/odw/moxy/moxy__rpt_sod_post_intraday_import_portfolios.sql
+++ models/odw/moxy/moxy__rpt_sod_post_intraday_import_portfolios.sql
@@ -2,6 +2,6 @@ select
     lower(portfolio_id)        as "PortID"
     , name                     as "PortName"
     , account_number           as "AcctID"
-    , 'None - Intraday Import' as "Error Type"
+    , 'None - Intraday Import' as "Error Type"--noqa:RF05
 from {{ ref('moxy__accounts_build_flat') }}
 where is_intraday_import = 1
