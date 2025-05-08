bb9affacc9e89b5685c19e4772cc10ed65d67fe0
Andrew Charlton <143548142+acharchar@users.noreply.github.com>
2025-05-08 16:19:02 -0500

    fix: [Envestnet Bills] corrects filename deduplication logic (#644)

--- models/base/pms/envestnet_manasquan/envestnet_manasquan__stg_bills.sql
+++ models/base/pms/envestnet_manasquan/envestnet_manasquan__stg_bills.sql
@@ -1,46 +1,40 @@
 select
-    'envestnet'                                    as system_name
-    , 'manasquan'                                  as system_instance
-    , concat(system_name , '__' , system_instance) as system_key
-    , 'mwa'                                        as firm_source
-    , upper(json:ACCOUNT_NUMBER::text(200))        as account_number_formatted
+    'envestnet'                                                              as system_name
+    , 'manasquan'                                                            as system_instance
+    , concat(system_name , '__' , system_instance)                           as system_key
+    , 'mwa'                                                                  as firm_source
+    , upper(json:ACCOUNT_NUMBER::text(200))                                  as account_number_formatted
     , regexp_replace(
         ltrim(upper(replace(
             trim(json:ACCOUNT_NUMBER::text(200)) , '-' , ''
         )) , '0')
         , '\\s{2,}'
         , ' '
-    )                                              as account_number
-    , json:ADVISOR_FEE::decimal(20 , 2)            as advisor_fee
-    , json:BATCH_NAME::varchar(200)                as batch_name
-    , json:BILLABLE_VALUE::decimal(20 , 2)         as billable_value
-    , json:BILLING_CYCLE::varchar(200)             as billing_cycle
-    , json:BILL_TYPE::varchar(200)                 as bill_type
-    , json:CLIENT_FEE::decimal(20 , 2)             as client_fee
-    , json:CUSTODIAN_ID::varchar(200)              as custodian_id
-    , json:DEBITED_ACCOUNT::varchar(200)           as debited_account
-    , json:DEBIT_CUSTODIAN::varchar(200)           as debit_custodian
-    , json:DEBIT_TYPE::varchar(200)                as debit_type
-    , json:FIRM_FEE::decimal(20 , 2)               as firm_fee
-    , json:INVOICE_DATE::date                      as invoice_date
-    , json:PERIOD_END::date                        as period_end_date
-    , json:TOT_FEE_AMT::decimal(20 , 2)            as total_fee_amount
-    , _id                                          as _id
-    , _created_at                                  as _created_at
-    , _box_file_id::varchar(200)                   as _box_file_id
-    , _box_file_name::varchar(200)                 as _box_file_name
+    )                                                                        as account_number
+    , json:ADVISOR_FEE::decimal(20 , 2)                                      as advisor_fee
+    , json:BATCH_NAME::varchar(200)                                          as batch_name
+    , json:BILLABLE_VALUE::decimal(20 , 2)                                   as billable_value
+    , json:BILLING_CYCLE::varchar(200)                                       as billing_cycle
+    , json:BILL_TYPE::varchar(200)                                           as bill_type
+    , json:CLIENT_FEE::decimal(20 , 2)                                       as client_fee
+    , json:CUSTODIAN_ID::varchar(200)                                        as custodian_id
+    , json:DEBITED_ACCOUNT::varchar(200)                                     as debited_account
+    , json:DEBIT_CUSTODIAN::varchar(200)                                     as debit_custodian
+    , json:DEBIT_TYPE::varchar(200)                                          as debit_type
+    , json:FIRM_FEE::decimal(20 , 2)                                         as firm_fee
+    , json:INVOICE_DATE::date                                                as invoice_date
+    , json:PERIOD_END::date                                                  as period_end_date
+    , json:TOT_FEE_AMT::decimal(20 , 2)                                      as total_fee_amount
+    , _id                                                                    as _id
+    , _created_at                                                            as _created_at
+    , _box_file_id::varchar(200)                                             as _box_file_id
+    , _box_file_name::varchar(200)                                           as _box_file_name
+    -- obtains the first part of the file name which is the date of record, deduplicate files downstream
+    , substring(_box_file_name , 1 , position('__' in _box_file_name))::text as _box_file_name_part_1
     , {{ col_is_head_with_partition(reference=src
-        , partition_col = '_box_file_name'
+        , partition_col = '_box_file_name_part_1'
         , reference_date_col='_created_at'
         , source_date_col='_created_at') }}
-    , case when max(_created_at)
-                over (
-                    partition by _box_file_name
-                    order by _created_at desc
-                )
-            = _created_at then 1
-        else 0
-    end::int                                       as _is_source_file_head
-    , _box_meta                                    as _box_meta
+    , _box_meta                                                              as _box_meta
 from {{ source('envestnet_mwa_raw','bills') }}
 where true
