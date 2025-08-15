3fabf76c01664683ce13a06ccf360822eb6a8b89
ianjhurst <139607457+ianjhurst@users.noreply.github.com>
2025-08-15 10:17:27 -0500

    fix: [Schwab Referral Fees] Accomodate 9-digit account number in schwab PQ/CQ files (#741)

--- models/base/custodian/schwab/base/schwab__base_referral_fees_advisor_network.sql
+++ models/base/custodian/schwab/base/schwab__base_referral_fees_advisor_network.sql
@@ -8,7 +8,7 @@ select
     , trim(substring(a.content , 10 , 8))        as trader_fee_account
     , trim(substring(a.content , 19 , 10))::date as transaction_date
     , substring(a.content , 30 , 8)              as fa_master
-    , substring(a.content , 39 , 8)              as account_number
+    , ltrim(substring(a.content , 39 , 8) , '0') as account_number
     , trim(substring(a.content , 48 , 30))       as client_name
     , trim(substring(a.content , 79 , 15))       as transaction_type
     , case
@@ -41,3 +41,54 @@ left join {{ ref('aux__stg_custodian_links') }} as cl
     between coalesce(cl.effective_start_date , a.effective_date) and coalesce(cl.effective_end_date , a.effective_date)
 left join {{ ref('custodian_firms') }} as cf
     on cl.firm_source = cf.firm_source
+where 1 = 1
+    and a.effective_date <= '8/6/2025'
+
+union all
+
+select
+    'schwab'                                     as custodian
+    , cl.firm_source                             as firm_source
+    , cf.firm                                    as firm
+    , trim(substring(a.content , 1 , 7))         as network_master
+    , trim(substring(a.content , 10 , 9))        as trader_fee_account
+    , trim(substring(a.content , 20 , 10))::date as transaction_date
+    , substring(a.content , 31 , 8)              as fa_master
+    , ltrim(substring(a.content , 40 , 9) , '0') as account_number
+    , trim(substring(a.content , 50 , 30))       as client_name
+    , trim(substring(a.content , 81 , 15))       as transaction_type
+    , case
+        when trim(substring(a.content , 114 , 15)) = '' then null
+        else trim(substring(a.content , 114 , 15))
+    end::decimal(16 , 2)                         as management_fee_amount
+    , case
+        when trim(substring(a.content , 114 , 15)) = '' then null
+        else trim(substring(a.content , 114 , 15))
+    end::decimal(5 , 2)                          as fee_percent
+    , case
+        when trim(substring(a.content , 130 , 16)) = '' then null
+        else trim(substring(a.content , 130 , 16))
+    end::decimal(16 , 2)                         as advisor_network_participation_fee
+    , case
+        when trim(substring(a.content , 147 , 16)) = '' then null
+        else trim(substring(a.content , 147 , 16))
+    end::decimal(16 , 2)                         as fee_paid_to_advisor
+    , left(a._source_file , 8)                   as master_number
+    , a.effective_date::date                     as effective_date
+    , {{ col_is_head(reference=source('schwab', 'referral_fees_advisor_network')) }}
+    , {{ col_is_current(date_col='effective_date') }}
+    , a._created_at::timestamp                   as _source_loaded_at
+    , a._source_file::varchar(100)               as _source_file
+from {{ source('schwab', 'referral_fees_advisor_network') }} as a
+left join {{ ref('aux__stg_custodian_links') }} as cl
+    on left(a._source_file , 8) = cl.link
+    and cl.custodian = 'schwab'
+    and a.effective_date
+    between coalesce(cl.effective_start_date , a.effective_date) and coalesce(cl.effective_end_date , a.effective_date)
+left join {{ ref('custodian_firms') }} as cf
+    on cl.firm_source = cf.firm_source
+where 1 = 1
+    -- Schwab increased from 8 digit to 10 (9 in this file) digit account numbers.
+    -- We load from single column content instead of from a pre-parsed csv because
+    -- this file type doesn't quote values with commas in them!
+    and a.effective_date >= '8/7/2025'
--- models/base/custodian/schwab/base/schwab__base_referral_fees_current_quarter.sql
+++ models/base/custodian/schwab/base/schwab__base_referral_fees_current_quarter.sql
@@ -1,49 +1,49 @@
 {{ config(tags=["referral_fees"]) }}
 
 select
-    'schwab'                                     as custodian
-    , cl.firm_source                             as firm_source
-    , cf.firm                                    as firm
-    , trim(substring(a.content , 1 , 7))         as quarter
-    , trim(substring(a.content , 9 , 10))::date  as report_run_date
-    , trim(substring(a.content , 20 , 10))::date as account_balance_as_of_date
-    , substring(a.content , 31 , 8)              as network_master
-    , substring(a.content , 40 , 8)              as fa_master
-    , substring(a.content , 49 , 9)              as household_id
-    , substring(a.content , 59 , 10)::date       as activation_date
+    'schwab'                                                  as custodian
+    , cl.firm_source                                          as firm_source
+    , cf.firm                                                 as firm
+    , trim(substring(a.content , 1 , 7))                      as quarter
+    , nullif(trim(substring(a.content , 9 , 10)) , '')::date  as report_run_date
+    , nullif(trim(substring(a.content , 20 , 10)) , '')::date as account_balance_as_of_date
+    , substring(a.content , 31 , 8)                           as network_master
+    , substring(a.content , 40 , 8)                           as fa_master
+    , substring(a.content , 49 , 9)                           as household_id
+    , nullif(trim(substring(a.content , 59 , 10)) , '')::date as activation_date
     , case
         when trim(substring(a.content , 70 , 10)) = '' then null
         else trim(substring(a.content , 70 , 10))
-    end::date                                    as termination_date
-    , substring(a.content , 81 , 8)              as account_number
-    , trim(substring(a.content , 90 , 30))       as client_name
-    , trim(substring(a.content , 134 , 10))      as billed_not_billed
+    end::date                                                 as termination_date
+    , ltrim(substring(a.content , 81 , 8) , '0')              as account_number
+    , trim(substring(a.content , 90 , 30))                    as client_name
+    , trim(substring(a.content , 134 , 10))                   as billed_not_billed
     , case
         when trim(substring(a.content , 121 , 12)) = '' then null
         else trim(substring(a.content , 121 , 12))
-    end::decimal(16 , 2)                         as account_daily_balance
+    end::decimal(16 , 2)                                      as account_daily_balance
     , case
         when trim(substring(a.content , 145 , 4)) = '' then null
         else trim(substring(a.content , 145 , 4))
-    end::int                                     as count_of_accounts_within_household
+    end::int                                                  as count_of_accounts_within_household
     , case
         when trim(substring(a.content , 150 , 12)) = '' then null
         else trim(substring(a.content , 150 , 12))
-    end::decimal(16 , 2)                         as household_ave_billable_assets
+    end::decimal(16 , 2)                                      as household_ave_billable_assets
     , case
         when trim(substring(a.content , 163 , 5)) = '' then null
         else trim(substring(a.content , 163 , 5))
-    end::decimal(5 , 4)                          as blended_tiered_rate
+    end::decimal(5 , 4)                                       as blended_tiered_rate
     , case
         when trim(substring(a.content , 169 , 12)) = '' then null
         else trim(substring(a.content , 169 , 12))
-    end::decimal(16 , 2)                         as fees_due_for_household
-    , left(a._source_file , 8)                   as master_number
-    , a.effective_date::date                     as effective_date
+    end::decimal(16 , 2)                                      as fees_due_for_household
+    , left(a._source_file , 8)                                as master_number
+    , a.effective_date::date                                  as effective_date
     , {{ col_is_head(reference=source('schwab', 'referral_fees_current_quarter')) }}
     , {{ col_is_current(date_col='effective_date') }}
-    , a._created_at::timestamp                   as _source_loaded_at
-    , a._source_file::varchar(100)               as _source_file
+    , a._created_at::timestamp                                as _source_loaded_at
+    , a._source_file::varchar(100)                            as _source_file
 from {{ source('schwab', 'referral_fees_current_quarter') }} as a
 left join {{ ref('aux__stg_custodian_links') }} as cl
     on left(a._source_file , 8) = cl.link
@@ -52,3 +52,65 @@ left join {{ ref('aux__stg_custodian_links') }} as cl
     between coalesce(cl.effective_start_date , a.effective_date) and coalesce(cl.effective_end_date , a.effective_date)
 left join {{ ref('custodian_firms') }} as cf
     on cl.firm_source = cf.firm_source
+where 1 = 1
+    and a.effective_date <= '8/6/2025'
+
+union all
+
+select
+    'schwab'                                                  as custodian
+    , cl.firm_source                                          as firm_source
+    , cf.firm                                                 as firm
+    , trim(substring(a.content , 1 , 7))                      as quarter
+    , nullif(trim(substring(a.content , 9 , 10)) , '')::date  as report_run_date
+    , nullif(trim(substring(a.content , 20 , 10)) , '')::date as account_balance_as_of_date
+    , substring(a.content , 31 , 8)                           as network_master
+    , substring(a.content , 40 , 8)                           as fa_master
+    , substring(a.content , 49 , 9)                           as household_id
+    , nullif(trim(substring(a.content , 59 , 10)) , '')::date as activation_date
+    , case
+        when trim(substring(a.content , 70 , 10)) = '' then null
+        else trim(substring(a.content , 70 , 10))
+    end::date                                                 as termination_date
+    , ltrim(substring(a.content , 81 , 9) , '0')              as account_number
+    , trim(substring(a.content , 91 , 30))                    as client_name
+    , trim(substring(a.content , 135 , 10))                   as billed_not_billed
+    , case
+        when trim(substring(a.content , 122 , 12)) = '' then null
+        else trim(substring(a.content , 122 , 12))
+    end::decimal(14 , 2)                                      as account_daily_balance
+    , case
+        when trim(substring(a.content , 146 , 4)) = '' then null
+        else trim(substring(a.content , 146 , 4))
+    end::int                                                  as count_of_accounts_within_household
+    , case
+        when trim(substring(a.content , 151 , 12)) = '' then null
+        else trim(substring(a.content , 151 , 12))
+    end::decimal(14 , 2)                                      as household_ave_billable_assets
+    , case
+        when trim(substring(a.content , 164 , 5)) = '' then null
+        else trim(substring(a.content , 164 , 5))
+    end::decimal(5 , 4)                                       as blended_tiered_rate
+    , case
+        when trim(substring(a.content , 170 , 12)) = '' then null
+        else trim(substring(a.content , 170 , 12))
+    end::decimal(14 , 2)                                      as fees_due_for_household
+    , left(a._source_file , 8)                                as master_number
+    , a.effective_date::date                                  as effective_date
+    , {{ col_is_head(reference=source('schwab', 'referral_fees_current_quarter')) }}
+    , {{ col_is_current(date_col='effective_date') }}
+    , a._created_at::timestamp                                as _source_loaded_at
+    , a._source_file::varchar(100)                            as _source_file
+from {{ source('schwab', 'referral_fees_current_quarter') }} as a
+left join {{ ref('aux__stg_custodian_links') }} as cl
+    on left(a._source_file , 8) = cl.link
+    and cl.custodian = 'schwab'
+    and a.effective_date
+    between coalesce(cl.effective_start_date , a.effective_date) and coalesce(cl.effective_end_date , a.effective_date)
+left join {{ ref('custodian_firms') }} as cf
+    on cl.firm_source = cf.firm_source
+where 1 = 1
+    -- Schwab increased from 8 digit to 10 (9 in this file) digit account numbers.
+    -- We load from single column content instead of from a pre-parsed csv because
+    -- this file type doesn't quote values with commas in them!
+    and a.effective_date >= '8/7/2025'
